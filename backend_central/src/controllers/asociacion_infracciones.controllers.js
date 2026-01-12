ba5736ee3ba20614989791ef832352da6fb4c3c6
crom <you@example.com>
2026-01-12 14:47:25 -0600

    agrega rollback funcionalidad para el modulo de insertar una infraccion

--- backend_central/src/controllers/asociacion_infracciones.controllers.js
+++ backend_central/src/controllers/asociacion_infracciones.controllers.js
@@ -1,16 +1,14 @@
-import { pool } from "../db.js";
 import { getInfraccionCatalogo } from "./catalogo.controllers.js";
 
 
-export const createAsociacionInfraccion = async (req, res,id_infraccion, infracciones) => {
+export const createAsociacionInfraccion = async (req, res, client, id_infraccion, infracciones) => {
 
     for (const articulo of infracciones) {
         try {
 
             const id_infraccionCatalogo = await getInfraccionCatalogo(req, res, articulo);
             if (!id_infraccion) {
-                console.error("ID de infracción no proporcionado");
-                return;
+                throw new Error("ID de infracción no proporcionado")
             }
 
             const query = `
@@ -22,11 +20,12 @@ export const createAsociacionInfraccion = async (req, res,id_infraccion, infracc
                 id_infraccion
             ];
 
-            await pool.query(query, values);
+            await client.query(query, values); // lo converti al cliente para poder hacer el rollback.
         } catch (error) {
             console.error("Error en createAsociacionInfraccion:", error.message);
             if (!res.headersSent) {
                 res.status(500).json({ error: "Error al asociar infracciones al folio" });
+                throw new Error("Error al asociar infracciones al folio")
             }
             return;
         }
