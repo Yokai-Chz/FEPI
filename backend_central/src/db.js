2cd5b92a2c5b636eea08d0e05cf908a3108729bf
Yokai-Chz <zekoopa@gmail.com>
2026-01-20 03:42:10 -0600

    Fix: Dockerfile

--- backend_central/src/db.js
+++ backend_central/src/db.js
@@ -1,18 +1,31 @@
 import pg from 'pg';
+import { DB_CONFIG } from './config.js';
 
-const connectionString = 'postgresql://postgres.zhwgmdxqzktfezpgbbdi:TheWeirdo08++@aws-0-us-west-2.pooler.supabase.com:6543/postgres'
+
+// const connectionString = 'postgresql://postgres.zhwgmdxqzktfezpgbbdi:TheWeirdo08++@aws-0-us-west-2.pooler.supabase.com:6543/postgres'
+
+// export const pool = new pg.Pool({
+//   connectionString: connectionString, 
+//   ssl: {
+//     rejectUnauthorized: false, 
+//   },
+// });
+
 
 export const pool = new pg.Pool({
-  connectionString: connectionString, 
-  ssl: {
-    rejectUnauthorized: false, 
-  },
+  user: DB_CONFIG.user,
+  host: DB_CONFIG.host,
+  database: DB_CONFIG.database,
+  password: DB_CONFIG.password,
+  port: DB_CONFIG.port  
 });
-
-pool.query('SELECT NOW()', (err, res) => {
+//Agrega un temporizador de 15 segundos para ejecutarlo
+setTimeout(() => {
+    pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('Error conectando a Base de Datos:', err);
   } else {
     console.log('Conectado con Base de datos exitosamente.');
   }
 });
+}, 15000);
