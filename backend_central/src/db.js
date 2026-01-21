95311a06bde13d83c1e6714e38629181825efb31
Yokai-Chz <zekoopa@gmail.com>
2026-01-20 23:01:21 -0600

    Fix: DB

--- backend_central/src/db.js
+++ backend_central/src/db.js
@@ -1,6 +1,17 @@
 import pg from 'pg';
 import { DB_CONFIG } from './config.js';
 
+
+// const connectionString = 'postgresql://postgres.zhwgmdxqzktfezpgbbdi:TheWeirdo08++@aws-0-us-west-2.pooler.supabase.com:6543/postgres'
+
+// export const pool = new pg.Pool({
+//   connectionString: connectionString, 
+//   ssl: {
+//     rejectUnauthorized: false, 
+//   },
+// });
+
+
 export const pool = new pg.Pool({
   user: DB_CONFIG.user,
   host: DB_CONFIG.host,
@@ -9,4 +20,13 @@ export const pool = new pg.Pool({
   port: DB_CONFIG.port,
 });
 
-
+//Agrega un temporizador de 15 segundos para ejecutarlo
+setTimeout(() => {
+    pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error conectando a Base de Datos:', err);
+  } else {
+    console.log('Conectado con Base de datos exitosamente.');
+  }
+});
+}, 15000);
